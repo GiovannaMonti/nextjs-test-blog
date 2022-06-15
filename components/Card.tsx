3b43b4c82a53e89a00e11f6570313d2f9cfaca37
Giovanna Monti <giovannamonti.ext@accurat.it>
2022-06-15 12:54:53 +0200

    Setup demo blog

---
+++ components/Card.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link"
+
+type Props = {
+  link: string
+  title: string
+  children?: JSX.Element
+}
+
+export const Card = (props: Props) => {
+  return (
+    <div className="card bg-base-100 shadow-xl">
+      <div className="card-body">
+        {/* Another small Next builtin component to allow page routing */}
+        <Link href={props.link}>
+          <a>
+            <h3 className="card-title text-primary">{props.title} &rarr;</h3>
+            <>{props.children}</>
+          </a>
+        </Link>
+      </div>
+    </div>
+  )
+}
